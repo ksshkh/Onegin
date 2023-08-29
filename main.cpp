b8019a019a51afb5d26a3a185d17f2da29d82dfe
Ksenia <ksukha.3.16@gmail.com>
2023-08-29 18:12:10 +0300

    fixed --use

--- main.cpp
+++ main.cpp
@@ -6,13 +6,25 @@
 #include "colors.h"
 
 int main(int argc, const char* argv[]) {
-  if (argc != 2) {
-    printf("Run programm again and enter --test, --use or --help");
+
+  if (argc == 1) {
+
+    solutions counted_roots = {};
+
+    printf(BLUE("Enter the coefficients a, b, c, to solve a quadratic equation of the form\nax^2 + bx + c = 0\n"));
+    printf(BLUE("(The program accepts only numbers)\n"));
+
+    coefficients_of_sq_equation coef = get_coeffs(stdin);
+
+    solve_of_sq_equation(coef, &counted_roots);
+
+    print_results_of_solving(counted_roots);
+
     return 0;
   }
 
-  if (strcmp(argv[1], "--help") == 0) {
-    printf("Enter --test to test the programm or --use to use it");
+  else if (strcmp(argv[1], "--help") == 0) {
+    printf("Enter --test to test the programm");
     return 0;
   }
 
@@ -21,19 +33,8 @@ int main(int argc, const char* argv[]) {
     return 0;
   }
 
-  else if (strcmp(argv[1], "--use") == 0) {
-
-    solutions counted_roots = {};
-
-    printf(BLUE("Enter the coefficients a, b, c, to solve a quadratic equation of the form\nax^2 + bx + c = 0\n"));
-    printf(BLUE("(The program accepts only numbers)\n"));
-
-    coefficients_of_sq_equation coef = save_coeffs(stdin);
-
-    solve_of_sq_equation(coef, &counted_roots);
-
-    print_results_of_solving(counted_roots);
-
+  else {
+    printf("Run programm again and enter --test or --help");
     return 0;
   }
 }
