c0a738f2c156e4d1b40863c705b3a66d0257b62a
Ksenia <ksukha.3.16@gmail.com>
2023-08-29 15:12:13 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -23,16 +23,16 @@ int main(int argc, const char* argv[]) {
 
   else if (strcmp(argv[1], "--use") == 0) {
 
-    solutions real_root = {};
+    solutions counted_roots = {};
 
     printf(BLUE("Enter the coefficients a, b, c, to solve a quadratic equation of the form\nax^2 + bx + c = 0\n"));
     printf(BLUE("(The program accepts only numbers)\n"));
 
-    coefficients_of_sq_equation coef = save_coefs_in_structure(stdin);
+    coefficients_of_sq_equation coef = save_coeffs(stdin);
 
-    solve_of_sq_equation(coef, &real_root);
+    solve_of_sq_equation(coef, &counted_roots);
 
-    print_results_of_solving(real_root);
+    print_results_of_solving(counted_roots);
 
     return 0;
   }
