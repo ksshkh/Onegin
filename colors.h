5ac8c081e6e305b909f077a30d748396a63b829d
Ksenia <ksukha.3.16@gmail.com>
2023-08-29 15:08:26 +0300

    guard

--- colors.h
+++ colors.h
@@ -1,4 +1,9 @@
+#ifndef COLORS
+#define COLORS
+
 #define RED(str) "\033[31m" str "\033[0m"
 #define GREEN(str) "\033[32m" str "\033[0m"
 #define PINK(str) "\033[35m" str "\033[0m"
 #define BLUE(str) "\033[36m" str "\033[0m"
+
+#endif
